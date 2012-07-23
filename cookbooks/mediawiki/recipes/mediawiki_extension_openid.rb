0e9b1f4477b00223597f07e9e907562cc897ec61
Bill Whitson <wcwhitson@gmail.com>
2012-07-23 01:00:08 -0700

    Extensions fixes

--- cookbooks/mediawiki/recipes/mediawiki_extension_openid.rb
+++ cookbooks/mediawiki/recipes/mediawiki_extension_openid.rb
@@ -12,8 +12,59 @@
 
 rightscale_marker :begin
 
-mediawiki_install_extension "OpenID" do
-     repo_git "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/OpenID.git"
+################################################################################
+# Install extension
+################################################################################
+# This extension is not installed with the usual mediawiki_install_extension
+# definition, due to additional steps in the install process.
+################################################################################
+ext_name = "OpenID"
+
+log "Install Extension #{ext_name}: Starting"
+
+install_dir = "#{node[:mediawiki][:installation_directory]}/#{node[:mediawiki][:extensions_dir]}/#{ext_name}"
+
+directory install_dir do
+  action :create
+end
+
+git install_dir do
+  repository "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/OpenID.git"
+  action :checkout
+end
+
+log "Install Extension #{ext_name}: Ending"
+
+execute "make" do
+  command "cd #{install_dir};make"
+  returns [0, 1]
+end
+
+
+################################################################################
+# Build configuration from templates
+################################################################################
+# Build LocalSettings.php from inputs.
+################################################################################
+log "Template config for #{ext_name}: Starting"
+
+log "Template config: Using template AddThisSettings.php.erb."
+template "#{node[:mediawiki][:installation_directory]}/#{node[:mediawiki][:ext_config_dir]}/#{ext_name}Settings.php" do
+  source "#{ext_name}Settings.php.erb"
+  mode "0644"
+  owner "root"
+  group "root"
+end
+
+log "Template config for #{ext_name}: Ending"
+
+################################################################################
+# Update the MediaWiki configuration
+################################################################################
+# Pick up the new extension
+################################################################################
+execute "update.php" do
+  command "php #{node[:mediawiki][:installation_directory]}/maintenance/update.php"
 end
 
 rightscale_marker :end
